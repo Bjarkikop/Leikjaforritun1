2c3527545ae119d7b3deef2e0069d911b22a60bd
Bjarkikop <42611493+Bjarkikop@users.noreply.github.com>
2020-01-27 17:21:48 +0000

    comitting game

---
+++ Verkefni1/Assets/Scripts/PLayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PLayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+    // Marked FixedUpdate because it alters physiscs
+    void FixedUpdate()
+    {
+        // add force of 2000 on the z-axis
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if ( Input.GetKey("d") ) 
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+         if ( Input.GetKey("a") ) 
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
